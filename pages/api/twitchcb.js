33645a9dc745bc8b9cfebae0bd6d62b96b8981eb
Anudit Nagar <nagaranudit@gmail.com>
2021-08-25 20:42:38 +0530

    Add Support for Twitch

--- pages/api/twitchcb.js
+++ pages/api/twitchcb.js
@@ -3,13 +3,10 @@ import fetch, { Headers } from 'node-fetch';
 
 export default async (req, res) => {
 
-    let { TWITCH_CLIENT_SECRET,  NEXT_PUBLIC_SITE_URL } = process.env;
+    const { TWITCH_CLIENT_SECRET,  NEXT_PUBLIC_SITE_URL } = process.env;
+    // console.log(TWITCH_CLIENT_SECRET,  NEXT_PUBLIC_SITE_URL);
 
     let { code }  = req.query;
-
-    console.log(req.query);
-
-    console.log(code);
 
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
@@ -19,7 +16,7 @@ export default async (req, res) => {
     urlencoded.append("client_secret", TWITCH_CLIENT_SECRET);
     urlencoded.append("code", code);
     urlencoded.append("grant_type", "authorization_code");
-    urlencoded.append("redirect_uri", NEXT_PUBLIC_SITE_URL + "/api/twitchcb");
+    urlencoded.append("redirect_uri", NEXT_PUBLIC_SITE_URL + '/api/twitchcb');
 
     let requestOptions = {
         method: 'POST',
@@ -33,10 +30,12 @@ export default async (req, res) => {
     let result = await response.json();
     // return res.status(200).json(result);
 
-    console.log(result)
+    // console.log(result);
 
     let h2 = new Headers();
+    h2.append("Accept", `application/vnd.twitchtv.v5+json`);
     h2.append("Authorization", `Bearer ${result?.access_token}`);
+    h2.append("Client-Id", `ghb2na9qbwpfvn2557fetvll6lxf1p`);
 
     let ro = {
         method: 'GET',
@@ -44,15 +43,14 @@ export default async (req, res) => {
         redirect: 'follow'
     };
 
-    let response2 = await fetch("https://api.twitch.tv/kraken/user", ro);
-    let spotifyData = await response2.json();
-    console.log(spotifyData)
+    let response2 = await fetch("https://api.twitch.tv/helix/users", ro);
+    let twitchData = await response2.json();
+    // console.log(twitchData);
 
     let { state : ethAddress } = req.query;
     // console.log(ethAddress);
-    // console.log(spotifyData);
 
-    await updateAuthData('twitch', ethAddress, spotifyData);
+    await updateAuthData('twitch', ethAddress, twitchData['data'][0]);
     return res.status(200).redirect('/');
 
 };
